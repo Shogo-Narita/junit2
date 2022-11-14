4ecaca73b1a247dee631d652b9bf63a16b137075
Shogo Narita <shogo.narita@rakus-partners.co.jp>
2022-11-14 16:19:52 +0900

    commit

---
+++ src/test/java/junit/tutorial/ex02/e04/ConsumptionTaxTest.java
@@ -0,0 +1,41 @@
+package junit.tutorial.ex02.e04;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class ConsumptionTaxTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	@ParameterizedTest
+	@CsvSource({
+		"5,100,105",
+		"5,3000,3150"
+		})
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
